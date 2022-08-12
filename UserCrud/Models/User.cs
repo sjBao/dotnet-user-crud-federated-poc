ef6dbba837a3be5ae37e93afe83a5c2f2d6b1f2e
sjbao <supuilam@gmail.com>
2022-08-12 13:55:22 -0400

    Add sqlite

--- UserCrud/Models/User.cs
+++ UserCrud/Models/User.cs
@@ -1,19 +1,41 @@
+using Microsoft.EntityFrameworkCore;
+using UserCrud.Models;
 namespace UserCrud.Models;
+
+
+
+public class UserContext : DbContext
+{
+    public DbSet<User> Users { get; set; }
+
+    public string DbPath { get; set; }
+
+    public UserContext() { 
+        var folder = Environment.SpecialFolder.LocalApplicationData;
+        var path = Environment.GetFolderPath(folder);
+        DbPath = System.IO.Path.Join(path, "users.db");
+    }
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+        => options.UseSqlite($"Data Source={DbPath}");
+}
+
+
+
 
 public class User
 {
-    public Guid Id { get; }
+    public Guid Id { get; set; }
     public string Username { get; set; }
     public string FirstName { get; set; }
     public string LastName { get; set; }
-    public DateTime DateCreated { get; }
+    public DateTime DateCreated { get; set; }
 
 
-    private User(
-        Guid id, 
-        string username, 
-        string firstName, 
-        string lastName, 
+    public User(
+        Guid id,
+        string username,
+        string firstName,
+        string lastName,
         DateTime dateCreated)
     {
         Id = id;
@@ -24,15 +46,15 @@ public class User
     }
 
     public static User Create(
-        string username, 
-        string firstName, 
+        string username,
+        string firstName,
         string lastName)
     {
         return new User(
-            Guid.NewGuid(), 
-            username, 
-            firstName, 
-            lastName, 
+            Guid.NewGuid(),
+            username,
+            firstName,
+            lastName,
             DateTime.UtcNow);
     }
 }
